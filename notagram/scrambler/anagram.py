39726da5f96a3d73e931e77807b196ea83e60f5a
byarker <byarker@protonmail.com>
2026-05-18 18:12:39 +0100

    Shuffle function fix

--- notagram/scrambler/anagram.py
+++ notagram/scrambler/anagram.py
@@ -6,5 +6,4 @@ def scramble(word: str) -> str:
     anagram = ""
     for i in range(1, length + 1):
         anagram += listword.pop(randint(0, length - i))
-        print(anagram)
     return anagram
