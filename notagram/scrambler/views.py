5c654cf16bb55a2faa6c4c6358310127cd04cd74
byarker <byarker@protonmail.com>
2026-05-18 17:13:48 +0100

    Changes prior to shuffle function

--- notagram/scrambler/views.py
+++ notagram/scrambler/views.py
@@ -1,12 +1,9 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
 
-'''
-def main(request):
-  template = loader.get_template('scrambler.html')
-  return HttpResponse(template.render())
-'''
 def main(request):
     return testing(request)
 
@@ -14,4 +11,7 @@ def scrambler(request):
     return render(request, 'scrambler.html')
 
 def testing(request):
-  return render(request, 'test.html')
+    context = {'word': '', 'prevwords': ['']}
+    if request.method == 'POST':
+        context['word'] = request.POST['word']
+    return render(request, 'test.html', context)
