939ae659db6cc0e8583413e10dc3b0eba2aeed20
byarker <byarker@protonmail.com>
2026-05-18 16:15:21 +0100

    Refactor to use shortcuts.render

--- notagram/scrambler/views.py
+++ notagram/scrambler/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
 
+'''
 def main(request):
   template = loader.get_template('scrambler.html')
   return HttpResponse(template.render())
+'''
+def main(request):
+    return scrambler(request)
 
 def scrambler(request):
-    template = loader.get_template('scrambler.html')
-    return HttpResponse(template.render())
+    return render(request, 'scrambler.html')
 
 def testing(request):
   template = loader.get_template('test.html')
